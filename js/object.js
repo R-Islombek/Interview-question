30fb269b6f587104db5d1ce743a2b33c48be5c20
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-10-28 09:06:49 +0500

    constructor funksiya

---
+++ js/object.js
@@ -0,0 +1,17 @@
+// let person = {
+//     id: 1,
+//     name: "Islombek",
+//     merried: true, 
+// }
+
+
+function Person(){
+    this.name = "Islombek";
+    this.age = 24;
+    this.id =  1
+}
+
+const user = new Person();
+
+
+console.log(user);
